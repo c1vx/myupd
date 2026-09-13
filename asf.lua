e3dacb1fbc2bc7eced84f174c887dab7ab2b1b92
c1vx <b85983617@gmail.com>
2026-09-13 21:47:25 +0200

    Refactor asf.lua to improve script structure

--- asf.lua
+++ asf.lua
@@ -1,16 +1,9 @@
--- Загружаем библиотеку из bib.lua
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/c1vx/myupd/main/bib.lua"))()
 
--- Создаем окно
 local mainWin = library:new({ name = "My Script Menu", color = Color3.fromRGB(225, 58, 81) })
-
--- Создаем вкладку
 local mainPage = mainWin:page({ name = "Главная" })
-
--- Создаем секцию
 local mainSec = mainPage:section({ name = "Функции" })
 
--- Добавляем кнопку
 mainSec:button({
     name = "Приветственное сообщение",
     callback = function()
@@ -18,7 +11,6 @@ mainSec:button({
     end
 })
 
--- Добавляем тумблер (Toggle)
 mainSec:toggle({
     name = "Тестовый Toggle",
     def = false,
